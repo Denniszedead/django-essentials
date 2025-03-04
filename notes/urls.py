8d86a069fa9f5cb7d614b814b62fa3de73d9b9f1
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-04 17:39:52 +0800

    Add link to go to the notes list

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
+    path('notes', views.NotesListView.as_view(), name='notes.list'),
     path('note/<int:pk>', views.NotesDetailView.as_view()),
     path('popular-notes', views.PopularNotesListView.as_view()),
 ]
