2214352dca652f50e86f26c900886bddda6739fd
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-04 17:53:51 +0800

    Add link for the notes details

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('notes', views.NotesListView.as_view(), name='notes.list'),
-    path('note/<int:pk>', views.NotesDetailView.as_view()),
+    path('note/<int:pk>', views.NotesDetailView.as_view(), name='notes.detail'),
     path('popular-notes', views.PopularNotesListView.as_view()),
 ]
