eef446cdd7e5e67ac5c33d8505bb880bc3fc7a52
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-04 11:45:50 +0800

    Create class based views for the authorized page

--- home/views.py
+++ home/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView;
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.now()}
 
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
