ddd13d3b150742f5b9c0589d15f98bc911ba411a
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-04 11:51:07 +0800

    Create class based views for the notes list page

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
+    path('notes', views.ListView.as_view()),
     path('notes/<int:pk>', views.detail),
 ]
