eecd94f88618962a615ff0b12c72085632575f8f
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-06 09:56:13 +0800

    Amend validation error note

--- notes/forms.py
+++ notes/forms.py
@@ -11,5 +11,5 @@ class NotesForm(forms.ModelForm):
     def clean_title(self):
         title = self.cleaned_data['title']
         if 'Django' not in title:
-            raise ValidationError('Django requires "title"')
+            raise ValidationError('We only accept notes about Django!')
         return title
