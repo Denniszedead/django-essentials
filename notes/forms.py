71628cbcaf3380000438e61cd2ba99186844cfc9
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-06 09:59:58 +0800

    Add styling for the notes form

--- notes/forms.py
+++ notes/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.template.defaultfilters import title
 
 from .models import Notes
 
@@ -7,6 +8,13 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ['title', 'text']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control my-5'}),
+            'text': forms.Textarea(attrs={'class': 'form-control mb-5'}),
+        }
+        labels = {
+            'text': 'Write your thoughts here:'
+        }
 
     def clean_title(self):
         title = self.cleaned_data['title']
