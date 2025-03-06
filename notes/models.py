1a901d0e8631fbffe3dc1717e7ac933a32d33e9d
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-06 17:25:47 +0800

    Update the notes models with user

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Notes(models.Model):
@@ -6,3 +7,4 @@ class Notes(models.Model):
     text = models.TextField()
     likes = models.PositiveSmallIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
