2d0e82550de31c220151476b2b3fa4b96784a03d
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-07 11:37:12 +0800

    Add logout views and it's respective template and urls

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import LogoutInterfaceView
 
 urlpatterns = [
     path('home/', views.HomeView.as_view()),
     path('authorized/', views.AuthorizedView.as_view()),
     path('login/', views.LoginInterfaceView.as_view()),
+    path('logout/', LogoutInterfaceView.as_view()),
 ]
