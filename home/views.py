a7fcd819db0799e103e9af51eb4833adbf8b26de
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-07 13:56:02 +0800

    Add view for SingUp

--- home/views.py
+++ home/views.py
@@ -1,10 +1,18 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect
 from datetime import datetime
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
