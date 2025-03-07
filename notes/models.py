6d33ba2d5d8ba83d465e18b0667510f9301011f2
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-07 11:07:35 +0800

    Amend is_public attribute name

--- notes/models.py
+++ notes/models.py
@@ -8,4 +8,4 @@ class Notes(models.Model):
     likes = models.PositiveSmallIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
-    isPublic = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False)
