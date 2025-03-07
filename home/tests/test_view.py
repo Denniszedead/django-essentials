0db7f1241a374769896bf81d167886d3fe706fe0
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-07 16:58:35 +0800

    Add test_signup_authenticated to test redirection if user enters signup page

--- home/tests/test_view.py
+++ home/tests/test_view.py
@@ -1,3 +1,7 @@
+import pytest
+
+from django.contrib.auth.models import User
+
 def test_home_view(client):
     response = client.get(path='/')
     assert response.status_code == 200
@@ -7,3 +11,13 @@ def test_signup(client):
     response = client.get(path='/signup')
     assert response.status_code == 200
     assert 'home/register.html' in response.template_name
+
+@pytest.mark.django_db
+def test_signup_authenticated(client):
+    user = User.objects.create_user('Dennis', 'dennisng2255@gmail.com', 'password')
+    client.login(username=user.username, password='password')
+
+    response = client.get(path='/signup', follow=True)
+
+    assert response.status_code == 200
+    assert 'notes/notes_list.html' in response.template_name
