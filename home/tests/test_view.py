9e5973f3f20cfb23c248d5508b760aced7db1f6e
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-07 16:51:31 +0800

    Add test_signup to test the signup page

--- home/tests/test_view.py
+++ home/tests/test_view.py
@@ -2,3 +2,8 @@ def test_home_view(client):
     response = client.get(path='/')
     assert response.status_code == 200
     assert 'Welcome to SmartNotes!' in str(response.content)
+
+def test_signup(client):
+    response = client.get(path='/signup')
+    assert response.status_code == 200
+    assert 'home/register.html' in response.template_name
