1eec11288a3c140b7e54bd01d134d8965015ca6d
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-07 11:34:29 +0800

    Create login page

--- home/views.py
+++ home/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
